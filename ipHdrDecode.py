3ce0d6665cc5292112b0dcc5f80986fe2368f4a2
ignacyexe <ignacy.dll@gmail.com>
2023-06-27 12:58:49 +0200

    Adding code that is decoding ICMP protocol

--- ipHdrDecode.py
+++ ipHdrDecode.py
@@ -39,6 +39,22 @@ class IP(Structure):
             self.protocol = str(self.protocol_num)
 
 
+class ICMP(Structure):
+    _fields_ = [
+        ("type", c_ubyte),
+        ("code", c_ubyte),
+        ("checksum", c_ushort),
+        ("unused", c_ushort),
+        ("next_hop_mtu", c_ushort)
+    ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+
+
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
 else:
@@ -58,6 +74,19 @@ try:
         ip_header = IP(raw_buffer[0:20])
 
         print(f"Protocol: {ip_header.protocol} {ip_header.src_address} -> {ip_header.dst_address}")
+
+        # decoding ICMP
+        if ip_header.protocol == "ICMP":
+            # reading beginning of ICMP packet
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            # creating ICMP structure
+            icmp_header = ICMP(buf)
+
+            print(f"ICMP -> Type: {icmp_header.type} Code: {icmp_header.code}")
+
+
 except KeyboardInterrupt:
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
