86abe4105471df2d60d36dcd7fcb23f081dc9bf6
ignacyexe <ignacy.dll@gmail.com>
2023-06-27 12:00:54 +0200

    I think I understood what's going with this sniffer

---
+++ UDP_sniffer.py
@@ -0,0 +1,25 @@
+import socket
+import os
+
+host = "192.168.172.210"
+
+# checking which os system does target use and assigning proper protocols to it
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer.bind((host, 0))
+
+# Catching also IP headers
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# Windows system is used, we must use IOCTL calling, so we can use unlimited mode
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+print(sniffer.recvfrom(65565))
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
